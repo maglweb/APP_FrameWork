856fc1f4425396d03cec57e9d3ce6fe8b8e1f0aa
magl <823358533@qq.com>
2017-12-26 22:42:16 +0800

    APP_FramWork

---
+++ APP_Frame/src/App/Util/Constant.java
@@ -0,0 +1,16 @@
+package App.Util;
+
+public class Constant {
+	// 定义用户名常量
+	public static final String UserName = "15911056310";
+	// 定义密码常量
+	public static final String Password = "111";
+	// 定义首页上方品牌列表常量 , "奥迪", "宝马", "别克", "奔驰", "现代", "日产", "更多"
+	public static final String[] Pinpai = {"大众", "丰田", "本田"};
+	// 定义首页快速筛选列表常量
+	public static final String[] QuickSearch = { "5万以下", "5-10万", "10-20万",
+			"20万以上", "SUV", "中型车", "紧凑型车", "小型车", "1年以内", "3年以内", "练手车",
+			"淘车认证车" };
+	// 当前版本
+	public static final String CurrentVersion = "V4.1.1";
+}
